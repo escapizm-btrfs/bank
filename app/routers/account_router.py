b5cb2ca281dffe3444b0fd5e8ccc400a60536a6b
escapizmm <brutdforce@gmail.com>
2026-04-29 15:29:50 +0000

    добавлена логика и ручка котороя возвращает счета пользователя

--- app/routers/account_router.py
+++ app/routers/account_router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from app.dependencies.Annotated import SessionDep, CurrentUserDep
-from app.services.account_service import create_account
 from app.schemas.account_schema import AccountCreateSchema
+
+from app.services.account_service import create_account, get_account
+
 
 
 
@@ -11,4 +13,8 @@ router = APIRouter()
 @router.post("/account")
 async def create_account_router(session:SessionDep, account: AccountCreateSchema,user:CurrentUserDep):
     return await create_account(session, account, user)
+
+@router.get("/account")
+async def get_account_router(session:SessionDep, user:CurrentUserDep):
+    return await get_account(session, user)
     
--- app/services/account_service.py
+++ app/services/account_service.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter, Depends
-
+from sqlalchemy import select
 
 from app.dependencies.Annotated import SessionDep, CurrentUserDep
-
-
 from app.models.account_model import AccountModel
-
 from app.schemas.account_schema import AccountCreateSchema
 
 
@@ -22,3 +19,10 @@ async def create_account(session:SessionDep, account:AccountCreateSchema, user:C
     session.add(new_account)
     await session.commit()
     return {"message": "account_created"}
+
+
+async def get_account(session:SessionDep, user:CurrentUserDep):
+    body_query = select(AccountModel).where(AccountModel.user_id == user)
+    query = await session.execute(body_query)
+    result = query.scalars().all()
+    return result
