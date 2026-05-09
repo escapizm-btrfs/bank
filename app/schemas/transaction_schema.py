22b80f8ad2b3ca93d554f95483f5857440a34f84
escapizm <brutdforce@gmail.com>
2026-05-09 08:32:04 +0300

    Добавил TransactionReadSchema

--- app/schemas/transaction_schema.py
+++ app/schemas/transaction_schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from decimal import Decimal
+from datetime import datetime
 
 
 
@@ -8,3 +9,8 @@ class TransactionCreateSchema(BaseModel):
     to_account_number: str
     amount: Decimal
 
+class TransactionReadSchema(BaseModel):
+    from_account_number: str
+    to_account_number: str
+    amount: Decimal
+    created_at: datetime
