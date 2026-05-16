6cfe306333116f76fa3a41cbbaa3202826a85b99
escapizm <brutdforce@gmail.com>
2026-05-16 22:03:19 +0500

    Доабвил тест на логин (запусать по одному тесту а то ловится loop issue)

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,14 @@ from app.main import app
 from app.models.user_model import UserModel
 from app.dependencies.Annotated import SessionDep
 
+@pytest.fixture(scope="session")
+def event_loop():
+    import asyncio
+
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close
+
 
 @pytest.mark.asyncio
 async def test_registration():
@@ -15,23 +23,20 @@ async def test_registration():
     async with AsyncClient(
         transport=transport,
         base_url="http://test"
-        
     ) as ac:
         response = await ac.post(
-            
             "/auth/registration",
             json={
-                "email": "brutdforce@gmail.com",
-                "name": "escapizm",
-                "password":"321321"
+                "email": "test_reg@test.com",
+                "name": "test_reg",
+                "password":"test"
             }
         )
-
         data = response.json()
-
         print(data)
+
         assert response.status_code == 200
-        assert data[1]["email"] == "brutdforce@gmail.com"
+        assert data[1]["email"] == "test_reg@test.com"
         assert "id" in data[1]
 
 
@@ -39,15 +44,49 @@ async def test_registration():
         error_responce = await ac.post(
             "/auth/registration",
             json = {
-                "email": "brutdforce@gmail.com",
-                "name": "escapizm",
-                "password":"321321"
+                "email": "test_reg@test.com",
+                "name": "test_reg",
+                "password":"test"
             }
         )
-
         assert error_responce.status_code == 400
 
         
          
+@pytest.mark.asyncio
+async def test_login():
+    transport = ASGITransport(app=app)
 
+    async with AsyncClient(
+        transport=transport,
+        base_url="http://test"
+    ) as ac:
+        create_user = await ac.post(
+            "/auth/registration",
+            json={
+                "email": "test_login@test.com",
+                "name": "test_login",
+                "password":"test"
+            }
+        )
+
+        data_user = create_user.json()
+
+        response = await ac.post(
+            "auth/login",
+            data={
+                "username": "test_login@test.com",
+                "password": "test"
+            }
+        )
+        data_login = response.json()
+
+        print(data_login)
+        assert response.status_code == 200
+
+        assert "access_token" in data_login
+
+
+
+        
         
